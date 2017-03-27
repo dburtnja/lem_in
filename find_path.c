7fec6b09108458c9a33dc7e7741d2d7a64ca5cca
denys <dostup.te@gmail.com>
2017-03-27 15:27:03 +0300

    start finding path

--- find_path.c
+++ find_path.c
@@ -4,11 +4,24 @@
 
 #include "lem_in.h"
 
+int		already_been_hear(int *path, int step, int nbr)
+{
+	while (step > 0)
+	{
+		if (path[step - 1] == nbr)
+			return (1);
+		step--;
+	}
+	return (0);
+}
+
 int		start_rec(int step, int *path, t_rooms *from, t_info *info)
 {
 	t_list	*p;
 
 	p = from->n_room;
+	if (already_been_hear(path, step, from->nbr) == 1)
+		return (0);
 	path[step] = from->nbr;
 	if (from->nbr == INT_MAX)
 	{
@@ -23,6 +36,27 @@ int		start_rec(int step, int *path, t_rooms *from, t_info *info)
 	return (0);
 }
 
+void	remove_duplicate(t_list *paths)
+{
+	t_list	*p;
+	int		i;
+	int		*tab;
+
+	i = 0;
+	p = paths;
+	while (p)
+	{
+		tab = (int*)p->content;
+		while (i < (int)p->content_size)
+		{
+			ft_printf("%d, ", tab[i]);
+			i++;
+		}
+		ft_putendl("");
+		p = p->next;
+	}
+}
+
 void	find_path(t_info *info)
 {
 	int		step;
@@ -33,4 +67,5 @@ void	find_path(t_info *info)
 	start_rec(step, path, info->start, info);
 	if (info->paths == NULL)
 		error(-10);
+	remove_duplicate(info->paths);
 }
