db128cb548a69d1c40cc3df14c892ff57c75ffa3
dburtnja <dostup.te@gmail.com>
2017-03-28 20:00:10 +0300

    move ants

--- move_ants.c
+++ move_ants.c
@@ -19,9 +19,43 @@ void	find_limit(t_list *paths, int first_len)
 	}
 }
 
+void	push_to_end(int *path, int path_size, t_info *info)
+{
+	path_size--;
+	while (path_size > 0)
+	{
+		if (path_size - 1 == 0 && info->start->ant > path[0])
+		{
+
+		}
+		path[path_size - 1]
+		path_size--;
+	}
+
+//	if (info->start->ant > path[0])
+}
+
+void	push_ants(t_info *info)
+{
+	t_list	*p;
+	int		*path;
+
+	p = info->paths;
+	while (info->start > 0)
+	{
+		while (p)
+		{
+			path = (int *) p->content;
+			push_to_end(path, (int)(p->content_size / sizeof(int)), info);
+			p = p->next;
+		}
+	}
+}
+
 void	move_ants(t_info *info)
 {
 	if (info->paths->next != NULL)
 		find_limit(info->paths->next,
 				   ((int)(info->paths->content_size / sizeof(int))));
+	push_ants(info);
 }
